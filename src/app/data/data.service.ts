3c7340ba06c6f6396baba1216361841ddce874b8
anirbmuk <anirbmuk@live.in>
2021-04-27 07:46:47 +0530

    fix: added missing states

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,48 +1,50 @@
 import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { combineLatest, fromEvent, Observable, of } from 'rxjs';
 import { delay, map, take } from 'rxjs/operators';
-
-import { IData } from './data.model';
-import { IFeatureState, isSearched, isAllLoaded, getLimitValue, getSkipValue } from './../scroll/state/feature.reducer';
+import { Store } from '@ngrx/store';
 import * as appActions from './../state/app.action';
 import * as featureActions from './../scroll/state/feature.action';
+import { IFeatureState, isSearched, isAllLoaded, getLimitValue, getSkipValue } from './../scroll/state/feature.reducer';
+
+import { IData } from './data.model';
 
 const sampleData: IData[] = [
   { id: 1, state: 'Andhra Pradesh', capital: 'Amaravati' },
   { id: 2, state: 'Arunachal Pradesh', capital: 'Itanagar' },
   { id: 3, state: 'Assam', capital: 'Dispur' },
   { id: 4, state: 'Bihar', capital: 'Patna' },
-  { id: 5, state: 'Goa', capital: 'Panaji' },
-  { id: 6, state: 'Haryana', capital: 'Chandigarh' },
-  { id: 7, state: 'Himachal Pradesh', capital: 'Shimla' },
-  { id: 8, state: 'Jharkhand', capital: 'Ranchi' },
-  { id: 9, state: 'Karnataka', capital: 'Bengaluru' },
-  { id: 10, state: 'Kerala', capital: 'Thiruvananthapuram' },
-  { id: 11, state: 'Madhya Pradesh', capital: 'Bhopal' },
-  { id: 12, state: 'Maharashtra', capital: 'Mumbai' },
-  { id: 13, state: 'Manipur', capital: 'Imphal' },
-  { id: 14, state: 'Meghalaya', capital: 'Shillong' },
-  { id: 15, state: 'Mizoram', capital: 'Aizawl' },
-  { id: 16, state: 'Nagaland', capital: 'Kohima' },
-  { id: 17, state: 'Odisha', capital: 'Bhubaneshwar' },
-  { id: 18, state: 'Punjab', capital: 'Chandigarh' },
-  { id: 19, state: 'Rajasthan', capital: 'Jaipur' },
-  { id: 20, state: 'Sikkim', capital: 'Gangtok' },
-  { id: 21, state: 'Tamil Nadu', capital: 'Chennai' },
-  { id: 22, state: 'Telangana', capital: 'Hyderabad' },
-  { id: 23, state: 'Tripura', capital: 'Agartala' },
-  { id: 24, state: 'Uttarakhand', capital: 'Dehradun' },
-  { id: 25, state: 'Uttar Pradesh', capital: 'Lucknow' },
-  { id: 26, state: 'West Bengal', capital: 'Kolkata' },
-  { id: 27, state: 'Andaman and Nicobar Islands', capital: 'Port Blair' },
-  { id: 28, state: 'Dadra & Nagar Haveli and Daman & Diu', capital: 'Daman' },
-  { id: 29, state: 'Jammu & Kashmir', capital: 'Srinagar & Jammu' },
-  { id: 30, state: 'Lakshadweep', capital: 'Kavaratti' },
-  { id: 31, state: 'Chandigarh', capital: 'Chandigarh' },
-  { id: 32, state: 'The Government of NCT of Delhi', capital: 'Delhi' },
-  { id: 33, state: 'Ladakh', capital: 'Leh' },
-  { id: 34, state: 'Puducherry', capital: 'Puducherry' }
+  { id: 5, state: 'Chattisgarh', capital: 'Raipur' },
+  { id: 6, state: 'Goa', capital: 'Panaji' },
+  { id: 7, state: 'Gujarat', capital: 'Gandhinagar' },
+  { id: 8, state: 'Haryana', capital: 'Chandigarh' },
+  { id: 9, state: 'Himachal Pradesh', capital: 'Shimla' },
+  { id: 10, state: 'Jharkhand', capital: 'Ranchi' },
+  { id: 11, state: 'Karnataka', capital: 'Bengaluru' },
+  { id: 12, state: 'Kerala', capital: 'Thiruvananthapuram' },
+  { id: 13, state: 'Madhya Pradesh', capital: 'Bhopal' },
+  { id: 14, state: 'Maharashtra', capital: 'Mumbai' },
+  { id: 15, state: 'Manipur', capital: 'Imphal' },
+  { id: 16, state: 'Meghalaya', capital: 'Shillong' },
+  { id: 17, state: 'Mizoram', capital: 'Aizawl' },
+  { id: 18, state: 'Nagaland', capital: 'Kohima' },
+  { id: 19, state: 'Odisha', capital: 'Bhubaneshwar' },
+  { id: 20, state: 'Punjab', capital: 'Chandigarh' },
+  { id: 21, state: 'Rajasthan', capital: 'Jaipur' },
+  { id: 22, state: 'Sikkim', capital: 'Gangtok' },
+  { id: 23, state: 'Tamil Nadu', capital: 'Chennai' },
+  { id: 24, state: 'Telangana', capital: 'Hyderabad' },
+  { id: 25, state: 'Tripura', capital: 'Agartala' },
+  { id: 26, state: 'Uttarakhand', capital: 'Dehradun' },
+  { id: 27, state: 'Uttar Pradesh', capital: 'Lucknow' },
+  { id: 28, state: 'West Bengal', capital: 'Kolkata' },
+  { id: 29, state: 'Andaman and Nicobar Islands', capital: 'Port Blair' },
+  { id: 30, state: 'Dadra & Nagar Haveli and Daman & Diu', capital: 'Daman' },
+  { id: 31, state: 'Jammu & Kashmir', capital: 'Srinagar & Jammu' },
+  { id: 32, state: 'Lakshadweep', capital: 'Kavaratti' },
+  { id: 33, state: 'Chandigarh', capital: 'Chandigarh' },
+  { id: 34, state: 'The Government of NCT of Delhi', capital: 'Delhi' },
+  { id: 35, state: 'Ladakh', capital: 'Leh' },
+  { id: 36, state: 'Puducherry', capital: 'Puducherry' }
 ];
 
 @Injectable()
@@ -76,6 +78,7 @@ export class DataService {
   );
 
   public fetchData(limit: number, skip: number): Observable<IData[]> {
+    console.log(`Fetching data between index ${skip} and ${limit}`);
     return of(sampleData.slice(skip, limit)).pipe(delay(1500), take(1));
   }
 
